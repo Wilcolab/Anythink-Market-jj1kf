00ebb60ce6484fcde57cb8daf1d7305f5ef2e593
go-dima <dima.gonikman@gmail.com>
2022-07-18 22:13:54 +0300

    Seed 100 users, items & comments

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,1 +1,91 @@
-//TODO: seeds script should come here, so we'll be able to put some data in our local env
+const mongoose = require('mongoose');
+var userLib = require("../models/User.js");
+var itemLib = require("../models/Item.js");
+var commentLib = require("../models/Comment.js");
+const User = mongoose.model("User", userLib.UserSchema);
+const Item = mongoose.model('Item', itemLib.ItemSchema);
+const Comment = mongoose.model('Comment', commentLib.CommentSchema);
+
+let mongo_uri = process.env.MONGODB_URI
+mongoose.connect(mongo_uri);
+
+function clearItems() {
+  return Item.deleteMany({'title': {$regex: /^Seed/}}).then(function() {
+    console.log("Done deleting items")
+  }).catch(function(error){
+    console.log("error deleting items")
+  })
+}
+
+function clearUsers() {
+  return User.deleteMany({'username': {$regex: /^johndoe/}}).then(function() {
+    console.log("Done deleting users")
+  }).catch(function(error){
+    console.log("error deleting users")
+  })
+}
+
+function clearComments() {
+  return Comment.deleteMany({'body': {$regex: /^.*$/}}).then(function() {
+    console.log("Done deleting comments")
+  }).catch(function(error){
+    console.log("error deleting comments")
+  })
+}
+
+function generateUsers() {
+  return Array.from(Array(100), (n, index) => {
+    let rand = Math.random() * Math.pow(36, 6);
+    let pipeZero = rand | 0;
+    let randSeed = pipeZero.toString(36);
+    return new User({
+      username: "JohnDoe" + index,
+      email: "jdoe" + index + "_" + randSeed + "@mymail.com"
+    })
+  });
+}
+
+function generateItems(users) {
+  return Array.from(Array(100), (n, index) => {
+    let rand = Math.random() * Math.pow(36, 6) | 0;
+    let userIdx = Math.random() * 100 | 0
+    let commentIdx = Math.random() * 100 | 0
+    return new Item({
+      title: "Seed item " + rand,
+      description: "Another seeded item!",
+      seller: users[userIdx].id,
+      // comments: [
+      //   new Comment({
+      //     body: "This blew my mind! " + rand,
+      //     seller: users[userIdx].id,
+      //   })
+      // ]
+    })
+  })
+}
+
+function generateComments(items) {
+  return Array.from(Array(100), (n, index) => {
+    let rand = Math.random() * Math.pow(36, 6) | 0;
+    // let userIdx = Math.random() * 100 | 0
+    let itemIdx = Math.random() * 100 | 0
+    return new Comment({
+      body: "This blew my mind! " + rand,
+      // seller: items[itemIdx].seller.id,
+      item: items[itemIdx].id
+    })
+  })
+}
+
+clearItems().then(clearUsers).then(clearComments).then(() => {
+  users = generateUsers()
+  User.insertMany(users).catch((err) => console.log("error!", err))
+}).then(() => {
+  items = generateItems(users)
+}).then(() => {
+  Item.insertMany(items).catch((err) => console.log("error!", err))
+}).then(() =>
+{
+  comments = generateComments(items)
+  Comment.insertMany(comments).then(() => console.log("Finished inserting comments"))
+}).then(() => process.exit(0))
