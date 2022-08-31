7b64a1a2c50ce2ed6ebf6ea39a7c8a1548a60511
go-dima <dima.gonikman@gmail.com>
2022-08-31 15:45:47 +0300

    Redux search

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,13 +1,35 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import { connect } from "react-redux";
+import { UPDATE_TITLE_SEARCH_TERM } from "../../constants/actionTypes";
 
-const Banner = () => {
+const Banner = (props) => {
+  const searchHandler = (ev) => {
+    ev.preventDefault();
+    const { value } = ev.target;
+    props.dispatch({
+      type: UPDATE_TITLE_SEARCH_TERM,
+      payload: { titleSearchTerm: value },
+    });
+  };
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
-        <div>
+        <div />
+        <div className="d-flex flex-row justify-content-center align-items-baseline">
           <span id="get-part">A place to get</span>
+          <form className="col-7">
+            <fieldset className="form-group">
+              <input
+                className="form-control"
+                id="search-box"
+                type="search"
+                placeholder="Item name placeholder"
+                onChange={searchHandler}
+              />
+            </fieldset>
+          </form>
           <span> the cool stuff.</span>
         </div>
       </div>
@@ -15,4 +37,4 @@ const Banner = () => {
   );
 };
 
-export default Banner;
+export default connect()(Banner);
--- frontend/src/reducers/itemList.js
+++ frontend/src/reducers/itemList.js
@@ -10,6 +10,7 @@ import {
   PROFILE_PAGE_UNLOADED,
   PROFILE_FAVORITES_PAGE_LOADED,
   PROFILE_FAVORITES_PAGE_UNLOADED,
+  UPDATE_TITLE_SEARCH_TERM,
 } from "../constants/actionTypes";
 
 const reducer = (state = {}, action) => {
@@ -80,6 +81,11 @@ const reducer = (state = {}, action) => {
     case PROFILE_PAGE_UNLOADED:
     case PROFILE_FAVORITES_PAGE_UNLOADED:
       return {};
+    case UPDATE_TITLE_SEARCH_TERM:
+      return {
+        ...state,
+        titleSearchTerm: action.payload.titleSearchTerm,
+      };
     default:
       return state;
   }
